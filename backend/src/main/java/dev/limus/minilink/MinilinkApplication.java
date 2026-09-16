5eb9b708e65abfbefe26ba8e47c264d81ce96d2c
Mustapha Limar <tapham079@gmail.com>
2026-09-16 23:00:06 +0100

    init

---
+++ backend/src/main/java/dev/limus/minilink/MinilinkApplication.java
@@ -0,0 +1,13 @@
+package dev.limus.minilink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinilinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinilinkApplication.class, args);
+	}
+
+}
