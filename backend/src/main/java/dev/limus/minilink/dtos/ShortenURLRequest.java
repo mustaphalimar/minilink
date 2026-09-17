3ffc64023cfa6647e045289213c1d625dfe4c82b
Mustapha Limar <tapham079@gmail.com>
2026-09-17 14:51:03 +0100

    ShortenURLRequest dto shape and validation

---
+++ backend/src/main/java/dev/limus/minilink/dtos/ShortenURLRequest.java
@@ -0,0 +1,23 @@
+package dev.limus.minilink.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortenURLRequest {
+    @NotBlank(message = "Original URL is required")
+    @Pattern(regexp = "^https?://.*", message = "URL must starts with http or https")
+    private String originURL;
+    private String customAlias;
+    private LocalDateTime expiresAt;
+
+}
